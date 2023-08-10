0f560478bb8fb58d0a87c9c78f8e7f281b7b54b1
Devon Thompson <88185903+dethompson92@users.noreply.github.com>
2023-08-10 01:37:19 -0400

    Update example-export.py

--- example-export.py
+++ example-export.py
@@ -8,7 +8,7 @@ from instastuff import *
 #
 # Example:
 #   target_user_id = '234567890'
-target_user_id = '<user-id-here>'
+target_user_id = '<432768477>'
 
 # Value of the `sessionid` cookie when logged into Instagram web.
 #
@@ -16,7 +16,7 @@ target_user_id = '<user-id-here>'
 #
 # Example: 
 #   session_id = '345678901%3AaGVsbG8geW91%3A1'
-session_id = '<session-id-here>'
+session_id = '<432768477%3AYCkLwMEyjeUoYX%3A0%3AAYcY05Z_wYoXTwk2mt89JcNNGRvX70DnmLhgZuhkrA>'
 
 # Run the exporter.
 #
